76630c7999878e11442faa6657db88c56b791613
Elvis McNeely <emcneely@norwex.com>
2016-01-06 11:06:32 -0800

    check

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -48,7 +48,7 @@
 	<body onload="playSong();">
 		<div class="container">
 			<div class="content">
-				<div class="title">Welcome My Love</div>
+				<div class="title">Welcome Valorie - Miss you babe!</div>
 				<div class="quote">{{ Inspiring::quote() }}</div>				<div >
 @for ($i = 0; $i < count($music); $i++)
   @if($i==0)
